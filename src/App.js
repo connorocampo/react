9c354361d6f2ae150e0fe485025eba27d9d52c6c
Connor Ocampo <connorocampo@gmail.com>
2018-11-02 15:01:47 -0400

    adding functionality

--- src/App.js
+++ src/App.js
@@ -31,7 +31,10 @@ class App extends Component {
     }
 
     handleAddProject(project){
-        console.log(project);
+        // console.log(project);
+        let projects = this.state.projects;
+        projects.push(project);
+        this.setState({projects:projects});
     }
 
     render() {
