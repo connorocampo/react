de0d201ea046b7f77e7380f8d00239ed0e430da6
Connor Ocampo <connorocampo@gmail.com>
2018-11-02 11:44:43 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
 import Projects from './Components/Projects';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            projects: []
+        }
+    }
+
+    componentWillMount(){
+        this.setState({projects: [
+            {
+                title: 'Business Website',
+                category: 'Web Design'
+            },
+            {
+                title: 'Social App',
+                category: 'Mobile Development'
+            },
+            {
+                title: 'Ecommerce Shopping Cart',
+                category: 'Web Development'
+            }
+        ]});
+    }
+
   render() {
+    console.log(this.props);
     return (
       <div className="App">
         My React App.
-        <Projects />
+        <Projects projects={this.state.projects} />
       </div>
     );
   }
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+import ProjectItem from './ProjectItem';
 // import logo from './logo.svg';
 // import './App.css';
 
 class Projects extends Component {
   render() {
+  let projectItems;
+  if(this.props.projects){
+      projectItems = this.props.projects.map(project => {
+            // console.log(project);
+            return (
+                <ProjectItem key={project.title} project={project} />
+            );
+        });
+  }
     return (
       <div className="Projects">
-        My Projects
+        {projectItems}
       </div>
     );
   }
