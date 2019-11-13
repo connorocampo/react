f9b6b44f5e774d66b2641bb487ab3f3a83133fa8
Cat Ocampo <cmosofla@gmail.com>
2019-11-12 20:33:18 -0500

    Updates

---
+++ precache-manifest.796a12fd24992a441d6b32bb78f16271.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d245ca9c7fbbb7b9a4a3d5440a9ce7f5",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "b19e518c06cc20c10be0",
+    "url": "/react-app/static/css/main.b52031d0.chunk.css"
+  },
+  {
+    "revision": "4ce38784d48905d10ece",
+    "url": "/react-app/static/js/2.628b60cd.chunk.js"
+  },
+  {
+    "revision": "b19e518c06cc20c10be0",
+    "url": "/react-app/static/js/main.b513daad.chunk.js"
+  },
+  {
+    "revision": "9b417d75f9f42c249c23",
+    "url": "/react-app/static/js/runtime-main.5f0f8821.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-app/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-app/precache-manifest.b924f38da26fc59d18607e9001a391e7.js"
+  "/react-app/precache-manifest.796a12fd24992a441d6b32bb78f16271.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-app/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-app/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
